9dbd80acf2d5c41c1b40a89cd532cd24653cd287
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-04-15 21:13:19 -0700

    Add login page

--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -3,7 +3,9 @@ const router = Router()
 
 // route for login.html
 router.get("/login", (req, res) => {
-    res.render('login');
+    const page_name = 'Login';
+    const registration = 'Register';
+    res.render('login', { page_name, registration });
 });
 
 export default router
--- src/server/routes/register.ts
+++ src/server/routes/register.ts
@@ -3,7 +3,8 @@ const router = Router()
 
 // route for registration.html
 router.get("/register", (req, res) => {
-    res.render('register');
+    const page_name = 'Registration'
+    res.render('register', { page_name });
 });
 
 export default router
