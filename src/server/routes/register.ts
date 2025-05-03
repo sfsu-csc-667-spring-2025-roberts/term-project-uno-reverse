9ae914ece8132279579f0a02a7d5b75b3ef0803d
Hluii <lui_hilary@yahoo.com>
2025-05-03 00:21:15 -0700

    Redirection on account cred is fin, ignore thee big uno reverse logo will fix when done w/ bg correction

--- src/server/routes/register.ts
+++ src/server/routes/register.ts
@@ -1,10 +1,35 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import pool from "../config/db";
+import bcrypt from "bcrypt";
+
 const router = Router()
 
 // route for registration.html
-router.get("/register", (req, res) => {
+router.get("/register", (req: Request, res: Response) => {
     const page_name = 'Registration'
     res.render('register', { page_name });
 });
+
+router.post('/register', async (req: Request, res: Response) => {
+    const { name, email, password } = req.body;
+    
+    console.log('Attempting to register user:', { name, email });
+
+    try {
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        await pool.query(
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',
+            [name, email, hashedPassword]
+        );
+
+        console.log('User registered successfully:', { name, email, hashedPassword });
+
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send('Error registering user');
+    }
+
+  });
 
 export default router
