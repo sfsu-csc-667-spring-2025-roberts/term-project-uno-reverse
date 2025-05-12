34b1cf5da56023a6e05277804944e8a263e67a22
thamizarasus <thamizarasus@gmail.com>
2025-05-11 19:37:21 -0700

    fixed app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname + "/views/public")));
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET!,
+    secret: process.env.SESSION_SECRET || "fallback-secret-key",
     resave: false,
     saveUninitialized: false,
   })
